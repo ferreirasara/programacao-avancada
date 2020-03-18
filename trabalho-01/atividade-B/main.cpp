ba46743e5822a1f7c8ae87ca79504b18820b2627
Sara Cristina Ferreira <ferreirasara1501@hotmail.com>
2020-03-18 17:49:51 -0300

    Atividade B

--- trabalho-01/atividade-B/main.cpp
+++ trabalho-01/atividade-B/main.cpp
@@ -7,18 +7,27 @@ float calculaMedia(int H, int P) {
     return (float)H / P;
 }
 
-int main(int argc, char* argv[]) {
+int main() {
     int H, P;
     cout << "Digite o total de cachorros-quentes: ";
     cin >> H;
 
+    if (H < 1 || H > 1000) {
+        cerr << "O total de cachorros-quentes deve ser um numero entre 1 e 1000. Valor digitado: " << H;
+        return EXIT_FAILURE;
+    }
+
     cout << "Digite o total de participantes: ";
     cin >> P;
 
+    if (P < 1 || P > 1000) {
+        cerr << "O total de participantes deve ser um numero entre 1 e 1000. Valor digitado: " << P;
+        return EXIT_FAILURE;
+    }
+
     float mediaConsumo = calculaMedia(H, P);
 
-    cout << fixed;
-    cout << "Numero medio de cachorros quentes consumidos: " << setprecision(2) << mediaConsumo;
+    cout << fixed << "Numero medio de cachorros quentes consumidos: " << setprecision(2) << mediaConsumo;
 
-	return 0;
+	return EXIT_SUCCESS;
 }
