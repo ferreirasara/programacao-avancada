b98e467f7ad4ee420731dddd48111990ba0dc0e8
Sara Cristina Ferreira <ferreirasara1501@hotmail.com>
2020-03-19 18:41:50 -0300

    Atividade B

--- trabalho-01/atividade-B/main.cpp
+++ trabalho-01/atividade-B/main.cpp
@@ -3,31 +3,31 @@
 
 using namespace std;
 
-float calculaMedia(int H, int P) {
+float calcAvg(int H, int P) {
     return (float)H / P;
 }
 
 int main() {
-    int H, P;
+    int totaOfHotDogs, totalOfParticipants;
     cout << "Digite o total de cachorros-quentes: ";
-    cin >> H;
+    cin >> totaOfHotDogs;
 
-    if (H < 1 || H > 1000) {
-        cerr << "O total de cachorros-quentes deve ser um numero entre 1 e 1000. Valor digitado: " << H << endl;
+    if (totaOfHotDogs < 1 || totaOfHotDogs > 1000) {
+        cerr << "O total de cachorros-quentes deve ser um numero entre 1 e 1000. Valor digitado: " << totaOfHotDogs << endl;
         return EXIT_FAILURE;
     }
 
     cout << "Digite o total de participantes: ";
-    cin >> P;
+    cin >> totalOfParticipants;
 
-    if (P < 1 || P > 1000) {
-        cerr << "O total de participantes deve ser um numero entre 1 e 1000. Valor digitado: " << P << endl;
+    if (totalOfParticipants < 1 || totalOfParticipants > 1000) {
+        cerr << "O total de participantes deve ser um numero entre 1 e 1000. Valor digitado: " << totalOfParticipants << endl;
         return EXIT_FAILURE;
     }
 
-    float mediaConsumo = calculaMedia(H, P);
+    float avgConsumption = calcAvg(totaOfHotDogs, totalOfParticipants);
 
-    cout << fixed << "Numero medio de cachorros quentes consumidos: " << setprecision(2) << mediaConsumo;
+    cout << fixed << "Numero medio de cachorros quentes consumidos: " << setprecision(2) << avgConsumption;
 
 	return EXIT_SUCCESS;
 }
