2f6831dac5c093af4a52af303b6c5251757b20c6
Sara Cristina Ferreira <ferreirasara1501@hotmail.com>
2020-03-19 18:38:32 -0300

    Atividade A

--- trabalho-01/atividade-A/main.cpp
+++ trabalho-01/atividade-A/main.cpp
@@ -2,24 +2,25 @@
 
 using namespace std;
 
-int calculaPrimeiroNumero(int N) {
+int calcFirstNumber(int N) {
     return N % 3;
 }
 
 int main() {
-    int N;
+    int maxNumber;
     cout << "Digite o valor de N: ";
-    cin >> N;
+    cin >> maxNumber;
+    
 
-    if (N < 3 || N > 10100) {
-        cerr << "O valor de N deve ser um numero entre 3 e 10100. Valor digitado: " << N << endl;
+    if (maxNumber < 3 || maxNumber > 10100) {
+        cerr << "O valor de N deve ser um numero entre 3 e 10100. Valor digitado: " << maxNumber << endl;
         return EXIT_FAILURE;
     }
 
-    int primeiroNumero = calculaPrimeiroNumero(N);
+    int firstNumber = calcFirstNumber(maxNumber);
 
-    if (primeiroNumero != 0) {
-        cout << "O primeiro jogador deve escolher " << primeiroNumero << " para ganhar." << endl;
+    if (firstNumber != 0) {
+        cout << "O primeiro jogador deve escolher " << firstNumber << " para ganhar." << endl;
     } else {
         cout << "O primeiro jogador nunca vai ganhar." << endl;
     }
